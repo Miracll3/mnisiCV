b6608d095c95b6f4acc56c72e53ed43bbab5b52b
Miracll3 <54441880+Miracll3@users.noreply.github.com>
2020-10-12 02:48:50 +0200

    load waiting

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ const factDiv = document.querySelector('.trivia p');
 form.addEventListener('click', (e)=>{
     e.preventDefault();
     const num = Math.floor(Math.random() * 301);
+    const loadText='Wait a little bit⌛';
+    factDiv.innerHTML=loadText;
     const baseURL= 'https://cors-anywhere.herokuapp.com/http://numbersapi.com/';
     fetch(baseURL+num,{
         method: "GET",
